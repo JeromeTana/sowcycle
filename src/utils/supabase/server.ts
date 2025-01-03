215b35fa460b50e8c15d2dd5c033f91535bd980a
Lostjerome <naronkrach@gmail.com>
2025-01-03 10:39:37 +0700

    More fix

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -8,12 +8,12 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
     {
       cookies: {
         getAll() {
-          return cookieStore.then((store) => store.getAll());
+          return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.then((store) => store.set(name, value, options))
+              cookieStore.set(name, value, options)
             );
           } catch {
             // The `setAll` method was called from a Server Component.
