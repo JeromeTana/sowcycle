fae35213e386536ca76ba9d9400aaaba589f1035
Lostjerome <naronkrach@gmail.com>
2025-01-03 16:34:03 +0700

    Add home page and test to read data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,44 @@
 import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
 import type { Sow } from "@/types/sow";
+import { Breeding } from "@/types/breeding";
+import Link from "next/link";
 
-const SowCard = ({ sow }: { sow: Sow }) => (
-  <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">{sow.name}</div>
-      <p className="text-gray-700 text-base">{sow.is_available}</p>
-      {/* Add more fields as needed */}
-    </div>
-  </div>
-);
+const SowCard = async ({ sow }: { sow: Sow }) => {
+  const supabase = createClient();
+  // get breedings for sow
+  let { data: breedings } = (await supabase
+    .from("breedings")
+    .select()
+    .eq("sow_id", sow.id)) as { data: Breeding[] };
+
+  return (
+    <Link
+      href={`/sows/${sow.id}`}
+      className="w-full max-w-sm rounded overflow-hidden shadow-lg border"
+    >
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{sow.name}</div>
+        <div>
+          {breedings && breedings.length > 0 ? (
+            <div>
+              <div className="font-bold text-xl mb-2">Breedings</div>
+              <div>
+                {breedings.map((breeding, index) => (
+                  <div key={index}>Breed at: {breeding.breed_date}</div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <div>No breedings</div>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 const SowList = ({ sows }: { sows: Sow[] }) => (
-  <div className="flex flex-wrap justify-center">
+  <div className="flex flex-col items-center justify-center gap-3">
     {sows.map((sow, index) => (
       <SowCard key={index} sow={sow} />
     ))}
@@ -21,10 +46,24 @@ const SowList = ({ sows }: { sows: Sow[] }) => (
 );
 
 export default async function Page() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
+  const supabase = createClient();
 
-  const { data: sows } = await supabase.from("Sows").select();
+  const { data: sows } = (await supabase.from("sows").select()) as {
+    data: Sow[];
+  };
+  const { data: breedings } = (await supabase.from("breedings").select()) as {
+    data: Breeding[];
+  };
 
-  return <SowList sows={sows!} />;
+  return (
+    <div>
+      {sows && <SowList sows={sows} />}
+      <div>
+        {breedings &&
+          breedings.map((breeding, index) => (
+            <div key={index}>{breeding.sow_id}</div>
+          ))}
+      </div>
+    </div>
+  );
 }
