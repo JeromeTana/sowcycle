4ba7955b355934477fe24d5dd3196e7122d33f61
Lostjerome <naronkrach@gmail.com>
2025-01-03 19:51:36 +0700

    Handle update

--- src/components/Sow/Form.tsx
+++ src/components/Sow/Form.tsx
@@ -2,36 +2,79 @@
 
 import { Sow } from "@/types/sow";
 import { createClient } from "@/utils/supabase/client";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default function SowForm() {
+export default function SowForm({ edittingSow }: { edittingSow?: Sow }) {
   const supabase = createClient();
   const [sow, setSow] = useState<Sow>({} as Sow);
+  const router = useRouter();
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSow({ ...sow, [e.target.name]: e.target.value });
   };
 
-  const handleCreate = async () => {
+  const createSow = async () => {
     try {
-      await supabase.from("sows").upsert([sow]);
-      window.location.reload();
+      const { data, error } = await supabase
+        .from("sows")
+        .insert([sow])
+        .select()
+        .single();
+      if (error) {
+        console.log(error);
+        return;
+      }
+      router.push(`/sows/${data.id}`);
     } catch (error: any) {
       console.log(error.message);
     }
   };
 
+  const updateSow = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("sows")
+        .update([sow])
+        .eq("id", sow.id)
+        .select()
+        .single();
+      if (error) {
+        console.log(error);
+        return;
+      }
+      router.push(`/sows/${data.id}`);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (edittingSow) {
+      updateSow();
+    } else {
+      createSow();
+    }
+  };
+
+  useEffect(() => {
+    if (edittingSow) {
+      setSow(edittingSow);
+    }
+  }, []);
+
   return (
-    <div className="text-black">
+    <div>
       <input
         type="text"
         name="name"
         placeholder="Name"
         onChange={onChange}
         className="border"
+        value={sow.name}
       />
-      <button className="bg-white" onClick={handleCreate}>
-        Create
+      <button onClick={handleSubmit}>
+        {edittingSow?.id ? "Update" : "Create"}
       </button>
     </div>
   );
