69cab30a11f9dd3cd9d6e32f5daed7ce6a70d5d1
Lostjerome <naronkrach@gmail.com>
2025-01-03 10:33:05 +0700

    Fix supabase server file

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
@@ -7,13 +7,13 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        async getAll() {
-          return (await cookieStore).getAll();
+        getAll() {
+          return cookieStore.then((store) => store.getAll());
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(async ({ name, value, options }) =>
-              (await cookieStore).set(name, value, options)
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.then((store) => store.set(name, value, options))
             );
           } catch {
             // The `setAll` method was called from a Server Component.
