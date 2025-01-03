981d713dd2d5b67741a7524d81d1182abf10b438
Lostjerome <naronkrach@gmail.com>
2025-01-03 16:49:39 +0700

    Add create sow form component

---
+++ src/components/Sow/Form.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Sow } from "@/types/sow";
+import { createClient } from "@/utils/supabase/client";
+import { useState } from "react";
+
+export default function SowForm() {
+  const supabase = createClient();
+  const [sow, setSow] = useState<Sow>({} as Sow);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSow({ ...sow, [e.target.name]: e.target.value });
+  };
+
+  const handleCreate = async () => {
+    try {
+      await supabase.from("sows").upsert([sow]);
+      window.location.reload();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="text-black">
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        onChange={onChange}
+        className="border"
+      />
+      <button className="bg-white" onClick={handleCreate}>
+        Create
+      </button>
+    </div>
+  );
+}
