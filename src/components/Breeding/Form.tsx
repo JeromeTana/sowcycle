aaffdf1f3637f06b1e207fcac9f4576e2aef6eb9
Lostjerome <naronkrach@gmail.com>
2025-01-05 19:39:34 +0700

    Edit styling

--- src/components/Breeding/Form.tsx
+++ src/components/Breeding/Form.tsx
@@ -387,62 +387,63 @@ export function FarrowForm({ breeding }: { breeding: Breeding }) {
                 </FormItem>
               )}
             />
-
-            <p className="text-sm">จำนวนลูกเกิด</p>
-            <div className="border p-4 rounded-lg space-y-4 bg-gray-50">
-              <div className="flex gap-2">
-                <FormField
-                  control={form.control}
-                  name="piglets_male_born_alive"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>ตัวผู้</FormLabel>
-                      <FormControl>
-                        <Input
-                          className="bg-white"
-                          type="number"
-                          min={0}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="piglets_female_born_alive"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>ตัวเมีย</FormLabel>
-                      <FormControl>
-                        <Input
-                          className="bg-white"
-                          type="number"
-                          min={0}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              <FormItem>
-                <FormLabel>รวม</FormLabel>
-                <FormControl>
-                  <Input
-                    className="bg-white"
-                    type="number"
-                    disabled
-                    value={totalBornPiglets}
-                    min={0}
+            <div className="space-y-2">
+              <p className="text-sm">จำนวนลูกเกิด</p>
+              <div className="border p-4 rounded-lg space-y-4 bg-gray-50">
+                <div className="flex gap-2">
+                  <FormField
+                    control={form.control}
+                    name="piglets_male_born_alive"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>ตัวผู้</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="bg-white"
+                            type="number"
+                            min={0}
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
+
+                  <FormField
+                    control={form.control}
+                    name="piglets_female_born_alive"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>ตัวเมีย</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="bg-white"
+                            type="number"
+                            min={0}
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <FormItem>
+                  <FormLabel>รวม</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      type="number"
+                      disabled
+                      value={totalBornPiglets}
+                      min={0}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              </div>
             </div>
 
             <FormField
