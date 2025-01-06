f5cfeeb5f115bb6469b8959cab99940c2d4e04f3
Lostjerome <naronkrach@gmail.com>
2025-01-06 09:11:59 +0700

    Add tabs

---
+++ src/components/TabsComponent.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "./ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+interface TabsComponentProps {
+  tabOptions: {
+    label: React.ReactNode;
+    value: string;
+    content: React.ReactNode;
+    default?: boolean;
+  }[];
+}
+
+export default function TabsComponent({ tabOptions }: TabsComponentProps) {
+  return (
+    <Tabs defaultValue={tabOptions.find((tab) => tab.default)?.value}>
+      <TabsList className={"flex w-full bg-gray-200"}>
+        {tabOptions.map((tab) => (
+          <TabsTrigger key={tab.value} value={tab.value} className="w-full">
+            {tab.label}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      <div className="mt-4" />
+      {tabOptions.map((tab) => (
+        <TabsContent key={tab.value} value={tab.value}>
+          {tab.content}
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
+}
+
+function AccountComponent() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Account</CardTitle>
+        <CardDescription>
+          Make changes to your account here. Click save when you're done.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" defaultValue="Pedro Duarte" />
+        </div>
+        <div className="space-y-1">
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" defaultValue="@peduarte" />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button>Save changes</Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+function PasswordComponent() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Password</CardTitle>
+        <CardDescription>
+          Change your password here. After saving, you'll be logged out.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
+          <Label htmlFor="current">Current password</Label>
+          <Input id="current" type="password" />
+        </div>
+        <div className="space-y-1">
+          <Label htmlFor="new">New password</Label>
+          <Input id="new" type="password" />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button>Save password</Button>
+      </CardFooter>
+    </Card>
+  );
+}
