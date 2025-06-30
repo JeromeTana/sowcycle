9c0a9932e039d4849df5fc284eb3597f8ac3c3bf
Jerome Tana <naronkrach@gmail.com>
2025-06-30 08:47:02 +0700

    fix: Optimize data fetching logic in BoarsPage component

--- src/app/boars/page.tsx
+++ src/app/boars/page.tsx
@@ -64,7 +64,7 @@ export default function BoarsPage() {
       setBoars(boars);
       setIsLoading(false);
     };
-    fetchData();
+    boars.length === 0 ? fetchData() : setIsLoading(false);
   }, []);
 
   if (isLoading) {
