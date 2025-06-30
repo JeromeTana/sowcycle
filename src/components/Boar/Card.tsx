1430cf311a0fcd5b549aec92d3b185cb76a6df79
Jerome Tana <naronkrach@gmail.com>
2025-06-30 08:16:06 +0700

    fix: Refactor BoarCard component to simplify structure and improve readability

--- src/components/Boar/Card.tsx
+++ src/components/Boar/Card.tsx
@@ -2,29 +2,20 @@
 
 import { Boar } from "@/types/boar";
 import Link from "next/link";
-import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "../ui/button";
-import { cn } from "@/lib/utils";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Dna } from "lucide-react";
 
 export default function BoarCard({ boar }: { boar: Boar }) {
-  const latestBreeding = boar.breedings?.[0];
-
   return (
-    <Card className={cn("w-full")}>
-      <CardHeader>
-        <CardTitle className="flex items-center gap-2">
-          <Dna />
-          {boar.breed}
-        </CardTitle>
-      </CardHeader>
-      <CardFooter>
-        <div className="w-full flex gap-2 justify-end">
-          <Link href={`/boars/${boar.id}`}>
-            <Button variant={"ghost"}>ดูรายละเอียด</Button>
-          </Link>
-        </div>
-      </CardFooter>
-    </Card>
+    <Link href={`/boars/${boar.id}`} className="w-full">
+      <Card className="shadow">
+        <CardHeader>
+          <CardTitle className="flex items-center gap-2">
+            <Dna />
+            {boar.breed}
+          </CardTitle>
+        </CardHeader>
+      </Card>
+    </Link>
   );
 }
